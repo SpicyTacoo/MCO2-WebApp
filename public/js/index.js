848ff0b8117a024b5cacfcc2207c57512d1a3a0d
Taco <bensonyao123@gmail.com>
2024-04-12 08:26:44 +0800

    change main page button redirect link

--- public/js/index.js
+++ public/js/index.js
@@ -2,28 +2,33 @@ const createAppointmentButton = document.getElementById("create-appointment");
 const checkAppointmentButton = document.getElementById("check-appointment");
 const updateAppointmentButton = document.getElementById("update-appointment");
 const cancelAppointmentButton = document.getElementById("cancel-appointment");
+let url
 
 createAppointmentButton?.addEventListener("click" , function(e) {
     console.log("Make An Appointment!")
-
-    location.href = "/make-appointment"
+    const portList = document.getElementById("port")
+    url = "/" + portList.value + "/make-appointment"
+    location.href = url
 })
 
 checkAppointmentButton?.addEventListener("click" , function(e) {
     console.log("Check An Appointment!")
-
-    location.href = "/check-appointment"
+    const portList = document.getElementById("port")
+    url = "/" + portList.value + "/make-appointment"
+    location.href = url
 })
 
 updateAppointmentButton?.addEventListener("click" , function(e) {
     console.log("Update An Appointment!")
-
-    location.href = "/update-appointment"
+    const portList = document.getElementById("port")
+    url = "/" + portList.value + "/make-appointment"
+    location.href = url
 })
 
 cancelAppointmentButton?.addEventListener("click" , function(e) {
-    console.log("Cancel An Appointment!")
-
-    location.href = "/cancel-appointment"
+    console.log("Delete An Appointment!")
+    const portList = document.getElementById("port")
+    url = "/" + portList.value + "/make-appointment"
+    location.href = url
 })
 
